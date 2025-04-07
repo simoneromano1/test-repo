3aa149abf3eea06f8da7e032e6f822e0cf2bd3be
Vvvale08 <rigamonti.valerie@gmail.com>
2025-04-07 20:03:51 +0200

    aggiunto file Indexx.html

--- html/obj/Debug/net9.0/html.AssemblyInfo.cs
+++ html/obj/Debug/net9.0/html.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("html")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1accfc7ed6cfa931ce88e832fe87701aec1f5eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ecffee3c4a32444742be774a1f4316e46ce3607")]
 [assembly: System.Reflection.AssemblyProductAttribute("html")]
 [assembly: System.Reflection.AssemblyTitleAttribute("html")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
